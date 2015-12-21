62b66d6f19456ce09980e2aeecb2b4ca5294c3ad
Jervis Muindi <jervismuindi@yahoo.co.uk>
2015-12-21 01:05:00 -0800

    Add file to contain constant literal values

---
+++ constants.go
@@ -0,0 +1,9 @@
+package mzika
+
+// Author: Jervis Muindi
+// Date: December 2015
+
+ 
+const kVideoId = "videoid"
+const kTableNameCachedVideo = "CachedVideos"
+const kTopVideosUrl = "https://api.vevo.com/mobile/v1/video/list.json?order=mostviewedthisweek&max=200"
