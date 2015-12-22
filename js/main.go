f8c171cb3a3e48100a98938dc1a38f634c60d7d8
Jervis Muindi <jervismuindi@yahoo.co.uk>
2015-12-22 10:34:03 -0800

    Expose methods for loading top videos and performing searches

--- js/main.go
+++ js/main.go
@@ -11,6 +11,8 @@ import (
 
 // We wrap the exported calls to JS in go-routine so that 
 // we avoid blocking. Output is returned via callback.
+// Also note when there is no error that happens in the callback,
+// the value of 'err' as seen by the JS  callback will be 'null'.
 
 // Returns a URL at which given video can be watched.
 func GetVideoUrl(vid string, callback* js.Object) {
@@ -43,17 +45,38 @@ func DecodeVideoJSON(input string, callback* js.Object) {
 	}()
 }
 
+func LoadTopVideoJSONListing(callback* js.Object) {
+	go func(){
+		listing_response, err := mzika.LoadTopVideoJSONListing()
+		if callback != nil {
+			callback.Invoke(listing_response, err)
+		}
+	}()
+}
+
+func LoadSearchedVideoJSONListing(query string, callback* js.Object) {
+	go func() {
+		listing_response, err := mzika.LoadSearchedVideoJSONListing(query)
+		if callback != nil {
+			callback.Invoke(listing_response, err)
+		}
+	}()
+}
+
 func main() {
 	// Specify the method that we want to make available to Javascript
 	// Our convention: all the method re-defined here are async by default (i.e. use goroutines). Methods under mzika.* can be blocking.
 	js.Global.Set("mzika", map[string]interface{} {
-		"decodeVideoJson": DecodeVideoJSON,
-		"decodeVideoJsonSync": mzika.DecodeVideoJSON,
+		"decodeVideoJSON": DecodeVideoJSON,
+		"decodeVideoJSONSync": mzika.DecodeVideoJSON,
 
 		"getVideoUrl" : GetVideoUrl,
 
 		"getVideoFromId" : GetVideoFromId,
 
+		"loadTopVideoJSONListing" : LoadTopVideoJSONListing,
+
+		"loadSearchedVideoJSONListing" : LoadSearchedVideoJSONListing, 
 		
 	})
 	fmt.Println("Hello, playground")
