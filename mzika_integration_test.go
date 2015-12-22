f4971d9e90dc0f5e47602ec9d5268ae08a0d4a63
Jervis Muindi <jervismuindi@yahoo.co.uk>
2015-12-22 09:48:31 -0800

    Add an integration test to verify logic of mzika is still valid/working

---
+++ mzika_integration_test.go
@@ -0,0 +1,28 @@
+package mzika_test
+
+// Integration test that is meant to verify whether logic in 
+// mzika package is still applicable/working properly. The test
+// cases here make actual network requests and so are suspecitible
+// to flaky failures.
+
+import (
+"strings"
+"testing"
+"github.com/jervisfm/mzika"
+)
+
+func TestGetVideoUrl(t *testing.T) {
+	vid := "uscmv1500002"
+	
+	url, err := mzika.GetVideoUrl(vid)
+	if err != nil {
+		// Test failed
+		t.Errorf("Failed to retrieve url for video with id '%s'. Error ", vid, err)
+	}
+	url = strings.ToLower(url)
+	if len(url) <= 0 || !strings.HasPrefix(url, "http") {
+		t.Errorf("Url obtained is invalid (either empty/not http url). Url: '%v'", url)
+	}
+	
+}
+
